5cf75ab21d3b2b0ac7d60a5670c671b34186878d
Tristan <29161072+trisbman@users.noreply.github.com>
2023-12-28 12:29:36 +0700

    other: remove `dotenv`

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 exports.handler = async (event) => {
     let response = "Hello from AWS!\nEvent: " + JSON.stringify(event, null, 2)
     
@@ -9,9 +7,6 @@ exports.handler = async (event) => {
 };
 
 (async () => {
-    // create `.env` file with `IS_DEVELOPMENT=TRUE` to
-    // log the function result to the console
-    if (!process.env.IS_DEVELOPMENT) return
     let params = {}
     const args = process.argv.slice(2)    
     if (args.length) params = JSON.parse(args)
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT
+const port = 8080
+
 const { handler } = require('./index')
 
 app.use(cors())
 app.get('/', async (req, res) => {    
     const response = await handler(req.query)
+    console.log(response)
     res.send(response)
 })
 
